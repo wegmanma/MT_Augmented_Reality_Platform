2025a1ebdc75e0880d6d4745d9b874224f469363
Marcel Wegmann (wegr) <wegr@zhaw.ch>
2021-11-17 15:48:38 +0100

    📝 Working on docu

--- 02_Source/python/visualization/sift_to_point_cloud.py
+++ 02_Source/python/visualization/sift_to_point_cloud.py
@@ -68,7 +68,8 @@ R_mat_2 = rotation_matrix([0, 1, 1], -0.1)
 for i in range(3):
     for j in range(4):
         cubefaces_rot[i][j] = np.matmul(R_mat_2, cubefaces[i][j])
-
+print("R_mat_2")
+print(R_mat_2)
 for i in range(3):
     for j in range(4):
         cubefaces_rot[i][j][1] += 0.1
@@ -132,13 +133,16 @@ patches.append(polygon)
 
 p = PatchCollection(patches, match_original=True, alpha=0.4)
 ax1.add_collection(p)
-for i in range(4):
-    ax1.scatter(points_0[i][0], points_0[i][1], color='#0465A9')
-    ax1.scatter(points_1[i][0], points_1[i][1], color='#0465A9')
-    ax1.scatter(points_2[i][0], points_2[i][1], color='#0465A9')
-    ax1.scatter(points_0_rot[i][0], points_0_rot[i][1], color='#A41F22')
-    ax1.scatter(points_1_rot[i][0], points_1_rot[i][1], color='#A41F22')
-    ax1.scatter(points_2_rot[i][0], points_2_rot[i][1], color='#A41F22')
+# for i in range(4):
+#     ax1.scatter(points_0[i][0], points_0[i][1], color='#0465A9')
+#     ax1.scatter(points_1[i][0], points_1[i][1], color='#0465A9')
+#     ax1.scatter(points_2[i][0], points_2[i][1], color='#0465A9')
+#     ax1.scatter(points_0_rot[i][0], points_0_rot[i][1], color='#A41F22')
+#     ax1.scatter(points_1_rot[i][0], points_1_rot[i][1], color='#A41F22')
+#     ax1.scatter(points_2_rot[i][0], points_2_rot[i][1], color='#A41F22')
+ax1.scatter(points_2[0][0], points_2[0][1], color='#0465A9')
+ax1.scatter(points_2[3][0], points_2[3][1], color='#0465A9')
+ax1.scatter(points_1[1][0], points_1[1][1], color='#0465A9')
 ax1.set_ylim(-1, 1)
 ax1.set_xlim(-1, 1)
 
@@ -204,7 +208,115 @@ ax21.add_collection3d(tri)
 ax21.scatter(0,0,0, color='#000000')
 for i in range(3):
     for j in range(4):
-        ax21.scatter(cubefaces[i][j][0],cubefaces[i][j][1],cubefaces[i][j][2], color='#0465A9')
-        ax21.scatter(cubefaces_rot[i][j][0],cubefaces_rot[i][j][1],cubefaces_rot[i][j][2], color='#A41F22')
-
-plt.show()
+        if i == 2:
+            if (j == 0) or (j == 3):
+                ax21.scatter(cubefaces[i][j][0],cubefaces[i][j][1],cubefaces[i][j][2], color='#0465A9')
+                ax21.scatter(cubefaces_rot[i][j][0],cubefaces_rot[i][j][1],cubefaces_rot[i][j][2], color='#A41F22')
+            else:
+                ax21.scatter(cubefaces[i][j][0],cubefaces[i][j][1],cubefaces[i][j][2], color='#000000')
+                ax21.scatter(cubefaces_rot[i][j][0],cubefaces_rot[i][j][1],cubefaces_rot[i][j][2], color='#000000')
+        if i == 1:
+            if (j == 1):
+                ax21.scatter(cubefaces[i][j][0],cubefaces[i][j][1],cubefaces[i][j][2], color='#0465A9')
+                ax21.scatter(cubefaces_rot[i][j][0],cubefaces_rot[i][j][1],cubefaces_rot[i][j][2], color='#A41F22')
+            if (j == 0) or (j == 3):
+                ax21.scatter(cubefaces[i][j][0],cubefaces[i][j][1],cubefaces[i][j][2], color='#000000')
+                ax21.scatter(cubefaces_rot[i][j][0],cubefaces_rot[i][j][1],cubefaces_rot[i][j][2], color='#000000')
+        if i == 0:
+             if (j == 0) or (j == 0) or (j == 0):
+                 ax21.scatter(cubefaces[i][j][0],cubefaces[i][j][1],cubefaces[i][j][2], color='#000000')
+                 ax21.scatter(cubefaces_rot[i][j][0],cubefaces_rot[i][j][1],cubefaces_rot[i][j][2], color='#000000')
+print("chosen0")
+print(cubefaces[2][0])
+print(cubefaces_rot[2][0])
+print("chosen1")
+print(cubefaces[2][3])
+print(cubefaces_rot[2][3])
+print("chosen2")
+print(cubefaces[1][1])
+print(cubefaces_rot[1][1])
+print("c_k")
+ck = np.zeros(3)
+ck[0] = 1/3*(cubefaces_rot[2][0][0]+cubefaces_rot[2][3][0]+cubefaces_rot[1][1][0])
+ck[1] = 1/3*(cubefaces_rot[2][0][1]+cubefaces_rot[2][3][1]+cubefaces_rot[1][1][1])
+ck[2] = 1/3*(cubefaces_rot[2][0][2]+cubefaces_rot[2][3][2]+cubefaces_rot[1][1][2])
+print(ck)
+print("c_k-1")
+ckm = np.zeros(3)
+ckm[0] = 1/3*(cubefaces[2][0][0]+cubefaces[2][3][0]+cubefaces[1][1][0])
+ckm[1] = 1/3*(cubefaces[2][0][1]+cubefaces[2][3][1]+cubefaces[1][1][1])
+ckm[2] = 1/3*(cubefaces[2][0][2]+cubefaces[2][3][2]+cubefaces[1][1][2])
+print(ckm)
+print("p_k")
+pk = np.zeros((3,3))
+pk[0][0]=cubefaces_rot[2][0][0]
+pk[0][1]=cubefaces_rot[2][3][0]
+pk[0][2]=cubefaces_rot[1][1][0]
+pk[1][0]=cubefaces_rot[2][0][1]
+pk[1][1]=cubefaces_rot[2][3][1]
+pk[1][2]=cubefaces_rot[1][1][1]
+pk[2][0]=cubefaces_rot[2][0][2]
+pk[2][1]=cubefaces_rot[2][3][2]
+pk[2][2]=cubefaces_rot[1][1][2]
+print(pk)
+print("p_k-1")
+pkm = np.zeros((3,3))
+pkm[0][0]=cubefaces[2][0][0]
+pkm[0][1]=cubefaces[2][3][0]
+pkm[0][2]=cubefaces[1][1][0]
+pkm[1][0]=cubefaces[2][0][1]
+pkm[1][1]=cubefaces[2][3][1]
+pkm[1][2]=cubefaces[1][1][1]
+pkm[2][0]=cubefaces[2][0][2]
+pkm[2][1]=cubefaces[2][3][2]
+pkm[2][2]=cubefaces[1][1][2]
+print("q_k")
+qk = np.zeros((3,3))
+qk[0][0]=cubefaces_rot[2][0][0]-ck[0]
+qk[0][1]=cubefaces_rot[2][3][0]-ck[0]
+qk[0][2]=cubefaces_rot[1][1][0]-ck[0]
+qk[1][0]=cubefaces_rot[2][0][1]-ck[1]
+qk[1][1]=cubefaces_rot[2][3][1]-ck[1]
+qk[1][2]=cubefaces_rot[1][1][1]-ck[1]
+qk[2][0]=cubefaces_rot[2][0][2]-ck[2]
+qk[2][1]=cubefaces_rot[2][3][2]-ck[2]
+qk[2][2]=cubefaces_rot[1][1][2]-ck[2]
+print(qk)
+print("q_k-1")
+qkm = np.zeros((3,3))
+qkm[0][0]=cubefaces[2][0][0]-ckm[0]
+qkm[0][1]=cubefaces[2][3][0]-ckm[0]
+qkm[0][2]=cubefaces[1][1][0]-ckm[0]
+qkm[1][0]=cubefaces[2][0][1]-ckm[1]
+qkm[1][1]=cubefaces[2][3][1]-ckm[1]
+qkm[1][2]=cubefaces[1][1][1]-ckm[1]
+qkm[2][0]=cubefaces[2][0][2]-ckm[2]
+qkm[2][1]=cubefaces[2][3][2]-ckm[2]
+qkm[2][2]=cubefaces[1][1][2]-ckm[2]
+print(qkm)
+print("S")
+S = np.matmul(qkm,np.transpose(qk))
+print(S)
+Us, Ss, VTs = np.linalg.svd(S)
+print("SVD")
+print(Us)
+print(Ss)
+print(np.transpose(VTs))
+S_det = np.identity(3)
+S_det[2][2] = np.linalg.det(np.matmul(np.transpose(VTs),np.transpose(Us)))
+R = np.matmul(np.transpose(VTs),np.matmul(S_det,np.transpose(Us)))
+print("matrix output")
+print(R)
+t = np.zeros(3)
+Rc = np.matmul(R,ckm)
+for i in range(3):
+    t[i] = ck[i]-Rc[i]
+print(t)
+pk_1 = np.matmul(R,(pkm))
+for i in range(3):
+    pk_1[i][0] = pk_1[i][0]+t[i]
+    pk_1[i][1] = pk_1[i][1]+t[i]
+    pk_1[i][2] = pk_1[i][2]+t[i]
+print(pk_1)
+print(pk)
+# plt.show()
